0832e99fb41decb816fd54df614e23228b018894
Ali Hassan <ahassan0213@gmail.com>
2024-09-04 06:36:02 -0400

    added main/specials section and css

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import HomePage from "./components/HomePage";
 import Nav from "./components/Nav";
 import Main from "./components/Main";
 import Footer from "./components/Footer"
+import { ChakraProvider } from '@chakra-ui/react'
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Nav/>
-      <HomePage/>
-      <Main/>
-      <Footer/>
-    </>
+    <ChakraProvider>
+      <>
+        <Nav/>
+        <HomePage/>
+        <Main/>
+        <Footer/>
+      </>
+    </ChakraProvider>
   );
 }
 
---
+++ src/components/MenuItem.js
@@ -0,0 +1,24 @@
+import { Card, CardBody, CardFooter, Image, Stack, Heading, Text, Divider, Button} from '@chakra-ui/react';
+
+function MenuItem(props){
+    return(
+      <Card maxW='sm' bg='gray.100'>
+        <CardBody>
+          <Image src={props.src} alt={props.alt} borderRadius='lg'/>
+            <Stack mt='6' spacing='3'>
+              <Heading size='md'>{props.heading}</Heading>
+              <Text>{props.text}</Text>
+              <Text color='yellow.600' fontSize='2xl'>${props.price}</Text>
+            </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+            <Button variant='ghost' colorScheme='black'>
+              Order a Delivery
+            </Button>
+        </CardFooter>
+      </Card>
+    );
+}
+
+export default MenuItem;
