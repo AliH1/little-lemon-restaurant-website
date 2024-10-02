0fa20e04f7a7d5263344364dbd4bf0adf32cdfe5
Ali Hassan <ahassan0213@gmail.com>
2024-10-02 18:11:32 -0400

    fixed secltion/opition for time and ocassion

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -18,19 +18,22 @@ function BookingForm(props){
 
   return(
     <section className="booking-form">
-      <form onsubmit={handleSubmit} className="booking-form-container">
+      <form onSubmit={handleSubmit} className="booking-form-container">
+        <h1>Book Your Table</h1>
         <label htmlFor="res-date">Choose date</label>
         <input type="date" id="res-date" onChange={handleDateChange} />
         <label htmlFor="res-time">Choose time</label>
-        <select id="res-time " onChange={(e) => setTime(e.target.value)}>
+        <select id="res-time" onChange={(e) => setTime(e.target.value)} required>
+          <option value="" disabled hidden>Select Time</option>
           {props.availableTimes.map(time => (
-            <opition key={time}>{time}</opition>
+            <option key={time}>{time}</option>
           ))}
         </select>
         <label htmlFor="guests">Number of guests</label>
         <input type="number" placeholder="1" min="1" max="10" id="guests" onChange={(e) => setGuests(e.target.value)}/>
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion" onChange={(e) => setOccasion(e.target.value)}>
+        <select id="occasion" onChange={(e) => setOccasion(e.target.value)} required>
+            <option value="" disabled hidden>Select Ocassion</option>
             <option>Birthday</option>
             <option>Anniversary</option>
         </select>
