aec5beb919a7a881f808e2ce283b3f502f289b7b
Ali Hassan <ahassan0213@gmail.com>
2024-10-02 19:08:34 -0400

    added unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import BookingForm from './components/BookingForm';
+import { initializeTimes, updateTimes } from './components/Main';
 
-test('Renders the BookingForm heading', () => {
-    render(<BookingForm />);
+test('Renders the BookingForm heading and reserve table button', () => {
+    render(<BookingForm availableTimes={[]} dispatch={expect.anything()} />);
     const headingElement = screen.getByText("Book Your Table");
     expect(headingElement).toBeInTheDocument();
+
+    const reserveButton = screen.getByText("Make Your Reservation");
+    expect(reserveButton);
+    fireEvent.click(reserveButton);
+})
+
+test('initializeTimes function and updateTimes function test', () => {
+    const expected = {times: ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']};
+    const intialState = initializeTimes();
+    const action = { type: 'action' };
+    expect(intialState).toEqual(expected);
+    const updatedState = updateTimes(expected, action);
+    expect (updatedState).toEqual(expected);
+
 })
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,15 @@ import {Routes, Route } from 'react-router-dom';
 import HomePage from "../pages/HomePage";
 import BookingPage from "../pages/BookingPage";
 
-function Main(){
+export function updateTimes(state, action){
+  return {times: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]};
+}
+export const initializeTimes = () => {
+  return {times: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]};
+};
 
-  function updateTimes(state, date){
-    return {times: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]};
-  }
-  const initial = {times: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]}
-  const [state, dispatch] = useReducer(updateTimes, initial);
+function Main(){
+  const [state, dispatch] = useReducer(updateTimes, initializeTimes());
 
   return(
     <Routes>
