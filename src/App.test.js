f5f4275b040387500cd0b4e8a88434362bf59559
Ali Hassan <ahassan0213@gmail.com>
2024-10-03 01:07:12 -0400

    added client-side valdiation, added some new unit tests, fixed some css

--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,10 @@ import BookingForm from './components/BookingForm';
 import { initializeTimes, updateTimes } from './components/Main';
 import { fetchAPI } from './api';
 
-test('Renders the BookingForm heading and reserve table button', () => {
+test('Renders the BookingForm heading', () => {
     render(<BookingForm availableTimes={[]} dispatch={expect.anything()} />);
     const headingElement = screen.getByText("Book Your Table");
     expect(headingElement).toBeInTheDocument();
-
-    const reserveButton = screen.getByText("Make Your Reservation");
-    expect(reserveButton);
-    fireEvent.click(reserveButton);
 })
 
 test('initializeTimes function and updateTimes function test', () => {
@@ -23,4 +19,16 @@ test('initializeTimes function and updateTimes function test', () => {
     const updatedState = updateTimes(intialState, new Date('2024-11-02'));
     expect(updatedState).not.toEqual(intialState);
 
+})
+
+test('client side validation test', () => {
+    render(<BookingForm availableTimes={[]} dispatch={expect.anything()} />);
+    const errorLabelOcassion = screen.getByText("Please select an occasion*");
+    expect(errorLabelOcassion);
+    const errorLabelDate = screen.getByText("Please choose a date*");
+    expect(errorLabelDate);
+    const errorLabelTime = screen.getByText("Please choose a time*");
+    expect(errorLabelTime);
+    const errorLabelGuests = screen.getByText("Input number of guests between 1-10*");
+    expect(errorLabelGuests);
 })
--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -2,20 +2,66 @@ import React, {useState} from "react";
 
 function BookingForm(props){
 
-  const [occasion, setOccasion] = useState("");
-  const [guests, setGuests] = useState("");
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("")
+  const [occasion, setOccasion] = useState({occasion: "", errorMessage: "Please select an occasion*",});
+  const [guests, setGuests] = useState({guests: "", errorMessage: "Input number of guests between 1-10*",});
+  const [date, setDate] = useState({date: "", errorMessage: "Please choose a date*",});
+  const [time, setTime] = useState({time: "", errorMessage: "Please choose a time*",});
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const booking = {occasion: occasion, guests: guests, date: date, time: time};
+    const booking = {occasion: occasion.occasion, guests: guests.guests, date: date.date, time: time.time};
     props.submit(booking);
   }
 
+  function validiateDate(e){
+    const today = new Date();
+    const date = new Date(e.target.value);
+    if(today > date)
+      return "Please pick a date that hasn't elapsed";
+    return "";
+  }
+
+  function validiateGuests(e){
+    if(e.target.value === "")
+      return "Input number of guests between 1-10*";
+    if(e.target.value < 1)
+      return "Number of guests cannot be lower than 1";
+    if(e.target.value > 10)
+      return "Maximum number of guests is 10";
+    return "";
+  }
+
+  function validateOcassion(e) {
+    if(e.target.value === "")
+      return "Please select an occasion*";
+    return "";
+  }
+
+  function validateTime(e) {
+    if(e.target.value === "")
+      return "Please choose a time*";
+    return "";
+  }
+
   const handleDateChange = (e) => {
-    setDate(e.target.value);
+    const error = validiateDate(e);
+    setDate({date: e.target.value, errorMessage: error});
     props.dispatch(e.target.value);
+  }
+
+  const handleGuestsChange = (e) => {
+    const error = validiateGuests(e);
+    setGuests({guests: e.target.value, errorMessage: error})
+  }
+
+  const handleOccasionChange = (e) => {
+    const error  = validateOcassion(e);
+    setOccasion({occasion: e.target.value, errorMessage: error})
+  }
+
+  const handleTimeChange = (e) => {
+    const error = validateTime(e);
+    setTime({time: e.target.value, errorMessage: error})
   }
 
   return(
@@ -24,22 +70,26 @@ function BookingForm(props){
         <h1>Book Your Table</h1>
         <label htmlFor="res-date">Choose date</label>
         <input type="date" id="res-date" onChange={handleDateChange} required/>
+        <label className="err-message">{date.errorMessage}</label>
         <label htmlFor="res-time">Choose time</label>
-        <select id="res-time" onChange={(e) => setTime(e.target.value)} required>
+        <select id="res-time" onChange={handleTimeChange} required>
           <option value="">--Select Time--</option>
           {props.availableTimes.map(time => (
             <option key={time}>{time}</option>
           ))}
         </select>
+        <label className="err-message">{time.errorMessage}</label>
         <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" onChange={(e) => setGuests(e.target.value)}/>
+        <input type="number" placeholder="1-10" min="1" max="10" id="guests" onChange={handleGuestsChange}/>
+        <label className="err-message">{guests.errorMessage}</label>
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion" onChange={(e) => setOccasion(e.target.value)} required>
+        <select id="occasion" onChange={handleOccasionChange} required>
             <option value="">--Select Ocassion--</option>
             <option>Birthday</option>
             <option>Anniversary</option>
         </select>
-        <button aria-label="make reservation" type="submit" className="submitBtn">Make Your Reservation</button>
+        <label className="err-message">{occasion.errorMessage}</label>
+        <button aria-label="make reservation" type="submit" className="submitBtn" disabled={!(date.errorMessage==="" && time.errorMessage==="" && occasion.errorMessage==="" && guests.errorMessage==="")}>Make Your Reservation</button>
       </form>
     </section>
   );
